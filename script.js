b026551a7f5ba467e73e5fac2009a2148c93c4ba
HromekFrantisek <hromekfrantisek2@gmail.com>
2020-09-21 09:59:23 +0200

    Monday time fix

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 
 function calculateTime() {
-    //Sunday fix (https://stackoverflow.com/questions/4156434/javascript-get-the-first-day-of-the-week-from-current-date)
     var currentDay = new Date();
     var day = currentDay.getDay() || 7; // Get current day number, converting Sun. to 7
-    if( day !== 1 )                     // Only manipulate the date if it isn't Mon.
-    
-    {
-        currentDay.setHours(-24 * (day - 1)); 
-    }    
 
     var nextLesson = new Date();
-    nextLesson.setDate(nextLesson.getDate());
+
+    //Pokud je pondeli po 11 hodine pridame k datumu tyden
+    if(day == 1 && currentDay.getHours() > 10) {
+
+        nextLesson.setDate(currentDay.getDate() + 7);
+    }
+
+    //Pokud je pondeli pred 11 hodinou nepridavame k datumu zadne dny
+    else if(day == 1 && currentDay.getHours() < 11) {
+        nextLesson.setDate(currentDay.getDate());
+    }
+
+    else if(day == 7) {
+        nextLesson.setDate(currentDay.getDate() + 1);
+    }
+    //Pokud neni pondeli pridame k datumu pocet dnu k dalsimu pondeli
+    else {
+        var diff = 7 - day + 1;
+        nextLesson.setDate(currentDay.getDate() + diff);
+    }
     nextLesson.setHours(11);
     nextLesson.setMinutes(0);
     nextLesson.setSeconds(0);
